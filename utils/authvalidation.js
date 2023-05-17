e7f72cb8a63e78f53af13a0d4b246c8f011c98a9
Rafsanjani N.I <45141323+maplezs@users.noreply.github.com>
2023-05-17 19:16:01 +0700

    Update authvalidation.js

--- utils/authvalidation.js
+++ utils/authvalidation.js
@@ -1,8 +1,8 @@
 const Joi = require('joi')
 
 const register = Joi.object().keys({
-  name: Joi.string().min(6).max(255).required(),
-  username: Joi.string().min(6).max(255).required(),
+  name: Joi.string().min(3).max(255).required(),
+  username: Joi.string().min(5).max(255).required(),
   email: Joi.string().min(6).max(255).required().email(),
   password: Joi.string().min(8).max(1024).required()
 })
