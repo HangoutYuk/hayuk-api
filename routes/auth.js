387860a2c8866a51ef0350660c2ff7e0e54139a7
ariqmuhammadsulthan <128428315+ariqmuhammadsulthan@users.noreply.github.com>
2023-05-18 18:30:37 +0700

    Update auth.js
    Add getUsers and getUser feature.

--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,11 @@ const route = require('express').Router()
 const validate = require('../middlewares/validation')
 const schema = require('../utils/authvalidation')
 // const Joi = require('joi')
-const { register, login } = require('../controllers/auth')
+const { register, login, getUsers, getUser, createUser, updateUser, deleteUser } = require('../controllers/auth')
 
 route.post('/register', validate(schema.register), register)
 route.post('/login', validate(schema.login), login)
+route.get('/users', getUsers)
+route.get('/users/:id', getUser)
 
 module.exports = route
