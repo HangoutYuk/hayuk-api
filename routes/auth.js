c7cd4de885cef7920b02afe230a93f561b098ea1
ariqmuhammadsulthan <128428315+ariqmuhammadsulthan@users.noreply.github.com>
2023-05-18 18:37:25 +0700

    Update auth.js
    Added createUser, updateUser, and deleteUser.

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,10 @@ const { register, login, getUsers, getUser, createUser, updateUser, deleteUser }
 
 route.post('/register', validate(schema.register), register)
 route.post('/login', validate(schema.login), login)
-route.get('/users', getUsers)
-route.get('/users/:id', getUser)
+route.get('/users', getUsers) // GET all users
+route.get('/users/:id', getUser) // GET a specific user based on ID
+route.post('/users', validate(schema.createUser), createUser)
+route.put('/users/:id', validate(schema.updateUser), updateUser)
+route.delete('/users/:id', validate(schema.deleteUser), deleteUser)
 
 module.exports = route
